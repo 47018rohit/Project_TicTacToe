c1e02f00d5498f67b443d37f76fc1bc8cb2b20ed
hakkanoodles <47018rohit@gmail.com>
2022-11-16 21:14:30 +0530

    pausing project need to figure out adding class to active button and computer choice

--- script/app.js
+++ script/app.js
@@ -1,8 +1,12 @@
-//board
-var board = (function(){
+
+var game = (function(){
+    let chooseButton = document.querySelectorAll('.choice');
+    let resetButton = document.querySelector('.reset');
+    let player; let computer; let initMarker;
     let gameBoard = document.querySelector('.gameBoard')
-    let cell;
-    
+    // let cell;
+
+    // board
     let boardObject ={
         cellArray: [1,2,3,4,5,6,7,8,9],
         populate: function(){
@@ -10,40 +14,39 @@ var board = (function(){
                 cell = document.createElement('div');
                 cell.classList.add('cell');
                 gameBoard.appendChild(cell);
+
+                cell.addEventListener('click', markerSet,{once:true})
+
             })
         }
-    }
-    
-    return {boardObject};
-})();
-
-board.boardObject.populate();
-
-
-var game = (function(){
-    let chooseButton = document.querySelectorAll('.choice');
-    let resetButton = document.querySelector('.reset');
-    let {boardObject} = board; let player; let computer; let initMarker;
         
+    }
+
     //player choice
     var playerChoice = function(e){
         player = e.target.innerHTML;
+        // e.classList.add('active');
+        console.log(player)
     }
 
     //computer choice
     var computerChoice = function(){
-
+        // computer
     }
-    //marker set
-    var computerChoice = function(){
-        
+    //marker set player
+    var markerSet = function(e){
+        e.target.innerHTML = player;
+    }
+    //marker set computer
+    var markerSetBot = function(e){
+        e.target.innerHTML = computer;
     }
     //switch turns
-    var computerChoice = function(){
+    var switchTurn = function(){
         
     }
     //reset
-    var computerChoice = function(){
+    var reset = function(){
         
     }
     //winner 
@@ -54,13 +57,14 @@ var game = (function(){
     //Events
         var events= {
             choice: function(){
-                chooseButton.forEach(button=> button.addEventListener('click', playerChoice))
+                // chooseButton.classList.('active');
+                chooseButton.forEach(button=> button.addEventListener('click', playerChoice));
             },
             computer: function(){
 
             },
             marker: function(){
-                
+                // boardObject.cellArray.forEach(cell => cell.addEventListener('click', markerSet,{once:true}))
             },
             switch:function(){
                 
@@ -69,7 +73,7 @@ var game = (function(){
 
             },
             reset: function(){
-            
+                resetButton.addEventListener('click', reset);
             }
         }
         return {events,boardObject};
@@ -78,3 +82,5 @@ var game = (function(){
 })();
 
 game.events.choice();
+game.events.marker();
+game.boardObject.populate();
